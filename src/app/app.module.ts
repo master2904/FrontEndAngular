bc69ad079cfbcdfcff622b4b49ed61c4a4ea25d0
Joel Gonzales <dark7master@gmail.com>
2024-01-31 23:02:58 -0400

    create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
+import { ToastrModule } from 'ngx-toastr';
+import { PruebaComponent } from './prueba/prueba.component';
+import { Prueba2Component } from './prueba2/prueba2.component';
+import { Prueba3Component } from './prueba3/prueba3.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PruebaComponent,
+    Prueba2Component,
+    Prueba3Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(), // ToastrModule added
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/prueba2/prueba2.component.ts
@@ -0,0 +1,11 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prueba2',
+  templateUrl: './prueba2.component.html',
+  styleUrls: ['./prueba2.component.css']
+})
+export class Prueba2Component {
+
+}
